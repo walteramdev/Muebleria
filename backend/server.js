b99ffd48e3926a8a0626d9302df1be17236020de
Walter Arias Molino <walter.am.dev@gmail.com>
2026-06-23 16:43:42 -0300

    fix:historial bug

--- backend/server.js
+++ backend/server.js
@@ -39,23 +39,28 @@ const seedCategories = async () => {
       const defaultCategories = [
         {
           name: "Outdoor",
-          image: "https://images.unsplash.com/photo-1540932239986-30128078f3c5?auto=format&fit=crop&w=800&q=80",
+          image:
+            "https://images.unsplash.com/photo-1540932239986-30128078f3c5?auto=format&fit=crop&w=800&q=80",
         },
         {
           name: "Living",
-          image: "https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?auto=format&fit=crop&w=800&q=80",
+          image:
+            "https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?auto=format&fit=crop&w=800&q=80",
         },
         {
           name: "Sofás",
-          image: "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?auto=format&fit=crop&w=800&q=80",
+          image:
+            "https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?auto=format&fit=crop&w=800&q=80",
         },
         {
           name: "Comedor",
-          image: "https://images.unsplash.com/photo-1577140917170-285929fb55b7?auto=format&fit=crop&w=800&q=80",
+          image:
+            "https://images.unsplash.com/photo-1577140917170-285929fb55b7?auto=format&fit=crop&w=800&q=80",
         },
         {
           name: "Dormitorio",
-          image: "https://images.unsplash.com/photo-1616594039964-ae9021a400a0?auto=format&fit=crop&w=800&q=80",
+          image:
+            "https://images.unsplash.com/photo-1616594039964-ae9021a400a0?auto=format&fit=crop&w=800&q=80",
         },
       ];
       await Category.insertMany(defaultCategories);
